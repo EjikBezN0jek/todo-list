02c66a1066292a75ae9035633c8f2d0becceb17a
Tanya Trushakova <tatianatrue04@gmail.com>
2022-01-19 21:06:38 +0300

    added design; added deleting tasks

--- src/script.js
+++ src/script.js
@@ -1,48 +1,82 @@
 const form = document.querySelector('.form');
 const formInput = document.querySelector('.form__input');
-const formBtn = document.querySelector('.form__btn');
 const tasksContainer = document.querySelector('.tasks__container');
 
-let list = [];
+let list = JSON.parse(localStorage.getItem('list')) || [];
+let currentId = list.length ? list[list.length - 1]?.id + 1 : 1;
 
-showTasks();
+showAllTasks();
+
+function saveTodos() {
+  localStorage.setItem('list', JSON.stringify(list))
+}
+
+function showAllTasks() {
+  tasksContainer.innerHTML = '';
+  list.forEach((task) => showTask(task.id, task.todo))
+}
+
+function getTask(id) {
+  return document.querySelector(`.task__item[data-id="${id}"]`);
+}
 
 function addTask(e) {
   e.preventDefault();
+  if (!formInput.value) return;
 
   let task = {
-    id: list.length + 1,
+    id: currentId++,
     todo: formInput.value,
     checked: false,
-    important: false
+    important: false,
   };
 
   list.push(task);
-  showTask(task.id, task.todo)
   formInput.value = '';
-}
-
-function showTasks() {
-  tasksContainer.innerHTML = '';
-  list.forEach((task) => showTask(task.id, task.todo))
-
-  const tasks = tasksContainer.querySelectorAll('.task__item')
-  tasks.forEach(task => task.addEventListener('click', () => checkTask(+task.dataset.id)))
+  showTask(task.id, task.todo)
+  saveTodos();
 }
 
 function showTask(id, todo) {
-  tasksContainer.insertAdjacentHTML("beforeend", `
-        <div class="task__item" data-id="${id}">${todo}</div>
-    `)
+  let wrapper = document.createElement('div');
+  wrapper.classList.add('task__item-wrap');
+
+  let removeBtn = document.createElement('button');
+  removeBtn.classList.add('task__btn', 'btn');
+  removeBtn.innerHTML = '<i class="fas fa-times"></i>'
+  removeBtn.addEventListener('click', () => removeTask(id))
+  wrapper.appendChild(removeBtn)
+
+  let taskItem = document.createElement('div');
+  taskItem.classList.add('task__item');
+  taskItem.innerText = todo;
+  taskItem.setAttribute('data-id', id)
+  taskItem.addEventListener('click', () => checkTask(id))
+  wrapper.appendChild(taskItem)
+
+  tasksContainer.appendChild(wrapper)
+
+  // Shorty:
+  // let removeBtn = `<button class="task__btn btn" onclick="removeTask(${id})"> <i class="fas fa-times"></i></button>`;
+  // let todoItem = `<div class="task__item" data-id="${id}" onclick="checkTask(${id})">${todo}</div>`;
+  // tasksContainer.insertAdjacentHTML("beforeend", `<div class="task__item-wrap">${removeBtn + todoItem}</div>`)
 }
 
 function checkTask(id) {
   const todo = list.find(item => item.id === id);
-  todo.checked = !todo.checked
+  todo.checked = !todo.checked;
 
-  const task = document.querySelector(`.task__item[data-id="${id}"]`);
-  task.classList.toggle('checked')
+  const task = getTask(id);
+  task.classList.toggle('checked');
+}
+
+function removeTask(id) {
+  list = list.filter(todo => todo.id !== id);
+  const task = getTask(id);
+  task.parentElement.remove();
+  saveTodos();
 }
 
 
 form.addEventListener('submit', addTask);
+
